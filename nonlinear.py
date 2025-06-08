ee0b67611d01e9763783773dd5a5166df7410665
cloner1998 <sajjadyami77@gmail.com>
2025-06-08 20:00:51 +0330

    initial dependent of initial conditions in non-linear-dynamic1 in certain values of mu

--- nonlinear.py
+++ nonlinear.py
@@ -30,3 +30,32 @@ non_linear = general_iteration(100, mu, 0.5, non_linear_dynamic1)
 draw_diagram(non_linear)
 
 '''the value mu = 0.75 is called critical value'''
+
+
+'''depending on initial conditions for different values of mu'''
+# you can see that by setting mu = 0.89 and just a tiny different in initial value there is no significant change
+mu: float = 0.89
+initial = fixed_point_non_linear_dynamic1(mu)
+print(initial)
+non_linear = general_iteration(100, mu, 0.5, non_linear_dynamic1)
+draw_diagram(non_linear)
+
+mu: float = 0.89
+initial = fixed_point_non_linear_dynamic1(mu)
+print(initial)
+non_linear = general_iteration(100, mu, 0.50001, non_linear_dynamic1)
+draw_diagram(non_linear)
+
+# but here now you can see by setting mu = 0.91 and tiny different in initial value there is significant change
+# in long term behavior
+mu: float = 0.91
+initial = fixed_point_non_linear_dynamic1(mu)
+print(initial)
+non_linear = general_iteration(100, mu, 0.5, non_linear_dynamic1)
+draw_diagram(non_linear)
+
+mu: float = 0.91
+initial = fixed_point_non_linear_dynamic1(mu)
+print(initial)
+non_linear = general_iteration(100, mu, 0.50001, non_linear_dynamic1)
+draw_diagram(non_linear)
